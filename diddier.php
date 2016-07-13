103f01e14943f1acba7345ff0349d1b8a0aa6bae
mcanwoo <super.can@bk.ru>
2016-07-13 23:13:13 +0300

    new

--- diddier.php
+++ diddier.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-  <title>Anasayfa</title>
+  <title>Fight</title>
   </head>
 <body>
 
@@ -9,78 +9,89 @@
 
 $name = $_GET["gender"];
 
+function conv($ada){
+    if ($ada==6 or $ada==1){
+        $neal='rock';
+        return $neal;
+    }
+    if ($ada==12 or $ada==2){
+        $neal='paper';
+        return $neal;
+    }
+    if ($ada==18 or $ada==3){
+        $neal='scissors';
+        return $neal;
+    }
+    if ($ada==24 or $ada==4){
+        $neal='lizard';
+        return $neal;
+    }
+    if ($ada==30 or $ada==5){
+        $neal='spock';
+        return $neal;
+    }
+}
 
-echo "you've  picked " . $name;
+echo "you've  picked " . conv($name);
 
 $pick = rand(1,5);
 
-echo "\n" . $pick;
+echo "<br/> cpu's picked ". conv($pick);
+//  echo "\n" . $pick;
 
-if ($pick == 1 and $name == rock){
-    echo "<br/> cpu's picked" . $name;
-    echo " <br/> noone wins try again";
-}
-if ($pick == 2 and $name == paper){
-    echo "<br/> cpu's picked" . $name;
-    echo " <br/> noone wins try again";
-}
-if ($pick == 3 and $name == scissors){
-    echo "<br/> cpu's picked" . $name;
-    echo " <br/> noone wins try again";
-}
-if ($pick == 4 and $name == lizard){
-    echo "<br/> cpu's picked" . $name;
-    echo " <br/> noone wins try again";
-}
-if ($pick == 5 and $name == spock){
-    echo "<br/> cpu's picked" . $name;
-    echo " <br/> noone wins try again";
-}
+$sum = $pick + $name;
+//$test = "http://media4.picsearch.com/is?_Y7hQeBvZU443SlMKzMN2d6GHg4LGm_5bj3RZ9Dbiqs&height=237";
 
-if ($pick == 1 and $name == paper){
-    echo "<br/> cpu's picked Rock ";
-    echo " <br/> you win";
+game($sum);
+ini_set('max_execution_time', 0);
+function game($dat){
+    $new =$dat%7;
+    if ( $new==0){
+        echo "<br/>noone wins try again";
+    }
+    if ($dat==29 or $dat==17 or $dat==22 or $dat==10 or $dat==33 or $dat==13 or $dat==31 or $dat==20 or $dat==26 or $dat==9) {
+
+        echo "<br/>Congrats, You Win!";
+
+    }
+    if ($dat==23 or $dat==11 or $dat==34 or $dat==16 or $dat==27 or $dat==19 or $dat==25 or $dat==8 or $dat==32 or $dat==15) {
+
+        echo "<br/>You Lost";
+    }
 }
 
-if ($pick == 1 and $name == scissors) {
-    echo "<br/> cpu's picked Rock ";
-    echo " <br/> you lost";
-}
-if ($pick == 1 and $name == lizard) {
-    echo "<br/> cpu's picked Rock ";
-    echo " <br/> you lost";
-    echo "<br> rock crushes lizard";
-}
-if ($pick == 1 and $name == spock) {
-    echo "<br/> cpu's picked Rock ";
-    echo " <br/> you lost";
-    echo "<br> spock vaporizes rock";
-}
-if ($pick == 2 and $name == rock) {
-    echo "<br/> cpu's picked paper ";
-    echo " <br/> you lost";
-    echo "<br> paper covers rock";
-}
-if ($pick == 2 and $name == scissors) {
-    echo "<br/> cpu's picked paper ";
-    echo " <br/> you win";
-    echo "<br> scissors cuts paper";
-}
-if ($pick == 2 and $name == lizard) {
-    echo "<br/> cpu's picked paper ";
-    echo " <br/> you won";
-    echo "<br> lizard eats paper";
+$urlm ="http://www.picsearch.com/index.cgi?q=".conv($pick);
+
+$url = "http://www.picsearch.com/index.cgi?q=".conv($name);
+
+$data = file_get_contents($url);
+
+$mydata =file_get_contents($urlm);
+
+function getir($baslangic, $son, $cekilmek_istenen)
+{
+    @preg_match_all('/' . preg_quote($baslangic, '/') .
+        '(.*?)'. preg_quote($son, '/').'/i', $cekilmek_istenen, $m);
+    return @$m[1];
 }
 
-if ($pick == 2 and $name == spock) {
-    echo "<br/> cpu's picked paper ";
-    echo " <br/> you lost";
-    echo "<br> paper disproves spock";
-}
+$title =explode('<img class="thumbnail" src="',$data);
+$title =explode('" alt="" data-width="299" data-height="224" id="QvERz0TT57RVcohjT-P_95N8X0UggslkzM_Aizknv2o" />',$title[1]);
+
+$testa = $title[0];
+
+
+$link =explode('<img class="thumbnail" src="',$data);
+$linka=explode('" alt="" data-width="299" data-height="224" id="QvERz0TT57RVcohjT-P_95N8X0UggslkzM_Aizknv2o" />',$link[1]);
+
+$test =$linka[0];
+
+//echo $linka[0];
 
  ?>
-
-
+<br/>
+<FORM><INPUT Type="button" VALUE="TRY AGAİN" onClick="history.go(-1);return true;"></FORM
+<br/>   <img src="<?php echo $test ?>" alt="Rules" style="width:276px;height:183px;"> <img src="<?php echo $testa ?>" alt="Rules" style="width:276px;height:183px;">
 
 </body>
 
--- index.php
+++ index.php
@@ -4,14 +4,17 @@
 <form action="diddier.php" method="get">
 
 
-    <input name="gender" type="radio" value="rock">Rock
-    <input name="gender" type="radio" value="paper">Paper
-    <input name="gender" type="radio" value="scissors">Scissors
-    <input name="gender" type="radio" value="lizard">Lizard
-    <input name="gender" type="radio" value="spock">Spock
+    <input name="gender" type="radio" value="6">Rock
+    <input name="gender" type="radio" value="12">Paper
+    <input name="gender" type="radio" value="18">Scissors
+    <input name="gender" type="radio" value="24">Lizard
+    <input name="gender" type="radio" value="30">Spock
 
 <input type="submit">
-    
+    <br/>  RULES
+
+    <br/>   <img src="http://in2.ccio.co/I7/y8/w4/274930752222258307Cn9S52uNc.jpg" alt="Rules" style="width:554px;height:443px;">
+
 </form>
 </body>
 </html>
